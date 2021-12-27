e9180b5f7afb4237567e192a3bc0da2b6827f496
Dan MacDonald <dan.b.macdonald@gmail.com>
2021-12-27 13:20:49 -0500

    add arduino code

---
+++ esp32/esp32.ino
@@ -0,0 +1,61 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "deadbeef-1fb5-459e-8fcc-555555555555"
+#define CHARACTERISTIC_UUID "b000b135-36e1-4688-b7f5-666666666666"
+#define LED_PIN 12
+
+void enableLED(bool en)
+{
+  digitalWrite(LED_PIN, (en ? HIGH : LOW));
+}
+
+BLECharacteristic *pCharacteristic = NULL;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  // set the led as output
+  pinMode(LED_PIN, OUTPUT);
+
+  BLEDevice::init("Fan Controller");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                   CHARACTERISTIC_UUID,
+                                   BLECharacteristic::PROPERTY_READ |
+                                   BLECharacteristic::PROPERTY_WRITE
+                                   );
+
+  pCharacteristic->setValue("Hello World says Dan");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+  std::string val = pCharacteristic->getValue();
+  Serial.print(" The Characteristic value is ");
+  Serial.println(val.c_str());
+  enableLED((val == "penis"));
+  
+}
